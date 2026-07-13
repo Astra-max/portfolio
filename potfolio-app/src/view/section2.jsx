6b3c08766de7abad4df8ee03293f6dd655669795
Astra-max <waoremaxwel17@gmail.com>
2026-07-13 18:37:21 +0300

    refactor: revamped skill section to circular progress bars

--- potfolio-app/src/view/section2.jsx
+++ potfolio-app/src/view/section2.jsx
@@ -8,7 +8,7 @@ export default function AboutMe()
         <div className="about-me-main">
             <div className="second-container">
                 <div className="with-image">
-                    <img src="my-photo.jpg" alt="me" className="image-direction" />
+                    <img src="astra.jpg" alt="me" className="image-direction" />
                     <div className='about-text'>
                         <p className='me'>About me</p>
                         <p className='text'>
